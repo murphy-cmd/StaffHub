a1035bc6bb5ff8487e6849f1176af53dbeb40b07
murphy-cmd <markjaysonmanzon514@gmail.com>
2026-07-06 10:00:26 +0800

    Implement sidebar toggle functionality
    Added event listeners for toggling and resizing the sidebar.

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,1 +1,36 @@
+// ==========================================
+// WorkSync Sidebar
+// ==========================================
 
+const sidebar = document.querySelector(".sidebar");
+const menuToggle = document.getElementById("menuToggle");
+
+// Toggle Sidebar
+menuToggle.addEventListener("click", () => {
+
+    // Desktop
+    if (window.innerWidth > 900) {
+
+        sidebar.classList.toggle("collapsed");
+
+    }
+
+    // Mobile
+    else {
+
+        sidebar.classList.toggle("show");
+
+    }
+
+});
+
+// Remove Mobile Sidebar kapag lumaki ulit ang screen
+window.addEventListener("resize", () => {
+
+    if (window.innerWidth > 900) {
+
+        sidebar.classList.remove("show");
+
+    }
+
+});
